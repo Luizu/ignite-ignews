8485b407129f010d62289ca508395a82b74e1958
luizu <luizucastilho@gmail.com>
2021-03-31 20:49:44 -0300

    Chore: Add link to redirect to postspage

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client'
+import Link from 'next/link';
 import { RichText } from 'prismic-dom'
 
 import styles from './styles.module.scss'
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
         <div className={styles.posts}>
 
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
 
         </div>
