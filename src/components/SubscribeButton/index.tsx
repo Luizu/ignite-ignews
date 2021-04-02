7564fddb847160ac301e79ee8cdc5f58e6fd20d6
luizu <luizucastilho@gmail.com>
2021-04-01 21:35:36 -0300

    Chore: Add a verification to subscription status

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/dist/client/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
@@ -9,10 +10,17 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
-    if(!session){
+    if (!session) {
       signIn('github')
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts')
+
       return;
     }
 
@@ -33,9 +41,9 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
   return (
     <button
-    type="button"
-    className={styles.subscribeButton}
-    onClick={handleSubscribe}
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
